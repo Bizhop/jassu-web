42a6cc2bdf7804625bda23b0b7d4a4d5d694e7b1
Ville Piispa <ville.piispa@gmail.com>
2021-02-12 10:01:50 +0200

    set websocket url with env var

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 const CleanWebpackPluginConfig = new CleanWebpackPlugin(["dist"], {})
 
 const WebSocketUrlPlugin = new webpack.DefinePlugin({
-  WEB_SOCKET_URL: process.env.NODE_ENV === "development" ? JSON.stringify('http://localhost:8181/refresh') : JSON.stringify('https://jassu-backend.herokuapp.com/refresh')
+  WEB_SOCKET_URL: JSON.stringify(process.env.JASSU_WEB_SOCKET_URL)
 })
 
 module.exports = {
